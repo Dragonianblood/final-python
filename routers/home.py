8f7e686330fe2f808b603f81146c957a6ecd5cbc
dragonianblood <adriaanjacobuscilliers@gmail.com>
2024-06-11 10:56:22 +0800

    added a class browser and a redirect to a html file minor issues fixed with colors

--- routers/home.py
+++ routers/home.py
@@ -11,12 +11,18 @@ templates = Jinja2Templates(directory="templates")
 async def get_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @router.get("/classes", response_class=HTMLResponse)
 async def get_classes(request: Request):
     return templates.TemplateResponse("class.html", {"request": request})
+
 
 @router.get("/calculator", response_class=HTMLResponse)
 async def get_calculator(request: Request):
     return templates.TemplateResponse("calculator.html", {"request": request})
 
 
+@router.get("/class/{class_id}", response_class=RedirectResponse)
+async def go_to_class(request: Request, class_id: str):
+    print(class_id)
+    return templates.TemplateResponse("calculator.html", {"request": request})
